731ba4b995a442245afa82833e938df0f721ed07
Gregory Shtrasberg <shtras@gmail.com>
2019-11-05 11:36:02 -0500

    Include cleanup

--- src/DB/DB.h
+++ src/DB/DB.h
@@ -3,18 +3,7 @@
 #include <string>
 #include <optional>
 
-#ifdef _MSC_VER
-#pragma warning(push, 0)
-#pragma warning(disable : 4265)
-#endif
-#include <bsoncxx/json.hpp>
-#include <bsoncxx/builder/stream/array.hpp>
-#include <bsoncxx/builder/stream/document.hpp>
-#include <bsoncxx/builder/stream/helpers.hpp>
-#include <bsoncxx/types.hpp>
-#ifdef _MSC_VER
-#pragma warning(pop)
-#endif
+#include "bsoncxx_wrap.h"
 
 namespace DB
 {
--- src/Engine/Closet.h
+++ src/Engine/Closet.h
@@ -6,19 +6,7 @@
 
 #include "rapidjson/stringbuffer.h"
 #include "rapidjson/writer.h"
-
-#ifdef _MSC_VER
-#pragma warning(push, 0)
-#pragma warning(disable : 4265)
-#endif
-#include <bsoncxx/json.hpp>
-#include <bsoncxx/builder/stream/array.hpp>
-#include <bsoncxx/builder/stream/document.hpp>
-#include <bsoncxx/builder/stream/helpers.hpp>
-#include <bsoncxx/types.hpp>
-#ifdef _MSC_VER
-#pragma warning(pop)
-#endif
+#include "bsoncxx_wrap.h"
 
 #include "World.h"
 #include "Card.h"
