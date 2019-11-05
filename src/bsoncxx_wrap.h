33ebca5e981c595d4c0abbcaaf6ec2e71dafaea5
Gregory Shtrasberg <shtras@gmail.com>
2019-11-05 14:53:50 -0500

    Added missing file

---
+++ src/bsoncxx_wrap.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#ifdef _MSC_VER
+#pragma warning(push, 0)
+#pragma warning(disable : 4265)
+#endif
+#include <bsoncxx/json.hpp>
+#include <bsoncxx/builder/stream/array.hpp>
+#include <bsoncxx/builder/stream/document.hpp>
+#include <bsoncxx/builder/stream/helpers.hpp>
+#include <bsoncxx/types.hpp>
+#ifdef _MSC_VER
+#pragma warning(pop)
+#endif
